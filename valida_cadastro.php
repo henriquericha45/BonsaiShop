65982752b037221134de68193aa6b6b0f9d9c8a6
henriquericha45 <henriquericha@hotmail.com>
2022-12-17 05:29:59 -0300

    atualizacao

--- valida_cadastro.php
+++ valida_cadastro.php
@@ -1,6 +1,54 @@
 <?php
-    if(isset($_POST['email']) && isset($_POST['senha'])) {
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(empty($_POST["nome"]) || empty($_POST["email"]) || empty($_POST["numero"]) || empty($_POST["senha"]) || empty($_POST["confirmaSenha"])) {
+        header('Location: cadastro.php?erro=1');
+    } else if(!empty($_POST["senha"]) && !empty($_POST["confirmaSenha"]) && $_POST["senha"] != $_POST["confirmaSenha"]) {
+        header('Location: cadastro.php?erro=2');
+    } else {
+        header('Location: home.php');
     }
+    /*
+    if (empty($_POST["nome"])) {
+        $nameErr = "* Nome obrigatório";
+    } else {
+        $name = test_input($_POST["nome"]);
+    }
+    #--
+    if (empty($_POST["email"])) {
+        $emailErr = "* Email obrigatório";
+    } else {
+        $email = test_input($_POST["email"]);
+    }
+    #--
+    if (empty($_POST["numero"])) {
+        $numberErr = "* Número obrigatório";
+    } else {
+        $number = test_input($_POST["numero"]);
+    }
+    #--
+    if (empty($_POST["senha"])) {
+        $passwordErr = "* Senha obrigatória";
+    } else {
+        $password = test_input($_POST["senha"]);
+    }
+    #--
+    if (empty($_POST["confirmaSenha"])) {
+        $confirmaSenhaErr = "* Confirmar senha obrigatória";
+    } else {
+        $confirmaPassword = test_input($_POST["confirmaSenha"]);
+    }
+
+    if ($password != $confirmaPassword) {
+        $confirmaSenhaErr = "* Senhas não conferem";
+    }
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    */
+}
+
 ?>
